5c876e022023c0c6d11b1923d253aaab0bc6fd16
Feras Jobeir <fjobeir@gmail.com>
2023-01-19 23:24:05 +0300

    Apply some form validation

--- components/RomanNumeralForm/RomanNumeralForm.tsx
+++ components/RomanNumeralForm/RomanNumeralForm.tsx
@@ -1,10 +1,40 @@
+import React, { useState } from "react"
+
 export default function RomanNumeralForm() {
+    const [userNumber, setUserNumber] = useState<number | null>(null)
+    const updateUserNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const n = +e.target.value
+        if (!n || isNaN(n)) {
+            setUserNumber(null)
+            return false
+        }
+        if (n < 1 || n > 1000) {
+            window.alert('Kindly choose a number between 1 and 1000 inclusive.')
+            return false
+        }
+        setUserNumber(n)
+    }
+    const handleUserKeyDowns = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        /**
+         * This regex will allow the user to use numbers and backspace keys only
+         * No (Ctrl+A) or Delete so far :/ 
+         */
+        if (!/^([0-9]|Backspace)$/.test(e.key)) {
+            e.preventDefault()
+            return false
+        }
+    }
     return (
         <>
             <p>Please type a number between 1 and 1000 to get the equivalent roman numeral</p>
-            <input type='number' min={1} max={1000}  />
+            <input type='number' min={1} max={1000} onChange={updateUserNumber} onKeyDown={handleUserKeyDowns} />
             <p>
-                Result: <small><i>Type a number above</i></small>
+                <>Result: {
+                    userNumber ?
+                        <>{userNumber}</> :
+                        <small><i>Type a number above</i></small>
+                }
+                </>
             </p>
         </>
     )
